14bbf8621c6adf9d18112a41bde81ede80efc375
vinu-129 <vinay.chudasama@1rivet.local>
2019-09-24 06:43:20 -0700

    static header,abouts us,contact us added

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 600) {
+        $('.top-nav').addClass('fixed-header');
+
+    }
+    else {
+        $('.top-nav').removeClass('fixed-header');
+    }
+});
+
+if ($(window).width() >= 728) {
+    $(".navbar-toggle").click(function (e) {
+        $(".nav").slideToggle(300);
+        e.preventDefault();
+    });
+
+}
